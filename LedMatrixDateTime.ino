a3ed393c7058b8c12aed27af9f97ca15dc7f25ef
Marek L <marekl@local>
2021-03-26 15:32:02 +0100

    Initial commit

---
+++ LedMatrixDateTime.ino
@@ -0,0 +1,143 @@
+#include <PxMatrix.h>
+
+#include <DateTime.h>
+#include <ESP8266WiFi.h>
+
+#define BLACK 0x0000
+#define BLUE 0x001F
+#define RED 0xF800
+#define GREEN 0x07E0
+#define CYAN 0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW 0xFFE0
+#define WHITE 0xFFFF
+
+#define WIFI_SSID "your_wifi_ssid"
+#define WIFI_PASS "your_wifi_password"
+
+#ifdef ESP32
+
+#define P_LAT 22
+#define P_A 19
+#define P_B 23
+#define P_C 18
+#define P_D 5
+#define P_E 15
+#define P_OE 2
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+#endif
+
+#ifdef ESP8266
+
+#include <Ticker.h>
+Ticker display_ticker;
+#define P_LAT 16
+#define P_A 5
+#define P_B 4
+#define P_C 15
+#define P_D 12
+#define P_E 0
+#define P_OE 2
+
+#endif
+// Pins for LED MATRIX
+
+//PxMATRIX display(32,16,P_LAT, P_OE,P_A,P_B,P_C);
+//PxMATRIX display(64,32,P_LAT, P_OE,P_A,P_B,P_C,P_D);
+PxMATRIX display(64,64,P_LAT, P_OE,P_A,P_B,P_C,P_D,P_E);
+
+#ifdef ESP8266
+// ISR for display refresh
+void display_updater()
+{
+  display.display(50);
+}
+#endif
+
+#ifdef ESP32
+void IRAM_ATTR display_updater(){
+  // Increment the counter and set the time of ISR
+  portENTER_CRITICAL_ISR(&timerMux);
+  //isplay.display(70);
+  display.displayTestPattern(70);
+  portEXIT_CRITICAL_ISR(&timerMux);
+}
+#endif
+
+
+void setup() {
+
+  Serial.begin(9600);
+  display.begin(32); // 32S screen.
+
+  display.flushDisplay();
+
+  setupWiFi();
+  setupDateTime();
+  Serial.println(DateTime.now());
+  Serial.println("--------------------");
+  Serial.println(DateTime.toString());
+  Serial.println(DateTime.toISOString());
+  Serial.println(DateTime.toUTCString());
+  Serial.println("--------------------");
+  Serial.println(DateTime.format(DateFormatter::COMPAT));
+  Serial.println(DateTime.format(DateFormatter::DATE_ONLY));
+  Serial.println(DateTime.format(DateFormatter::TIME_ONLY));
+  Serial.println("--------------------");
+  DateTimeParts p = DateTime.getParts();
+  Serial.printf("%04d/%02d/%02d %02d:%02d:%02d %ld (%s)\n", p.getYear(),
+                p.getMonth(), p.getMonthDay(), p.getHours(), p.getMinutes(),
+                p.getSeconds(), p.getTime(), p.getTimeZone());
+  Serial.println("--------------------");
+  
+
+  #ifdef ESP8266
+    display_ticker.attach(0.004, display_updater);
+  #endif
+
+  #ifdef ESP32
+    timer = timerBegin(0, 80, true);
+    timerAttachInterrupt(timer, &display_updater, true);
+    timerAlarmWrite(timer, 4000, true);
+    timerAlarmEnable(timer);
+  #endif
+
+  delay(1000);
+}
+
+void setupWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.println(millis());
+  Serial.print("WiFi Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+}
+
+void setupDateTime() {
+  DateTime.setServer("ntp.aliyun.com");
+
+  DateTime.setTimeZone("CET-1");
+  DateTime.begin();
+  if (!DateTime.isTimeValid()) {
+    Serial.println("Failed to get time from server.");
+  }
+}
+
+void loop() {
+
+  display.clearDisplay();
+  display.setTextColor(GREEN);
+  display.setCursor(2,18);
+  display.print(DateTime.format(DateFormatter::DATE_ONLY));
+  display.setTextColor(RED);
+  display.setCursor(8,38);
+  display.print(DateTime.format(DateFormatter::TIME_ONLY));
+  
+  delay(100);
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# LED Matrix DateTime
+
+This sketch shows current date and time on a 64x64 LED Matrix using Adafruit GFX library.
+
+It is developed for WeMos D1 mini (ESP8266) and 64x64 (32S) LED Matrix, but can be easily adjusted to run on an ESP32 and a matrix of other dimensions.
+
+First install the ESP8266 board using these instructions:
+https://github.com/esp8266/Arduino
+
+This Arduino IDE sketch needs the following libraries:
+
+* PxMatrix LED MATRIX library
+* Adafruit GFX Library
+* ESPDateTime
